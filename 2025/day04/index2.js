234b85bf8c5b093a7362c8ab543294d95b8454be
Guido Barbieri Pederiva <gbarbieripederiva@gmail.com>
2025-12-04 08:49:56 -0300

    feat(2025): add solutions for day 4 (both parts)

---
+++ 2025/day04/index2.js
@@ -0,0 +1,45 @@
+// Find problem at: https://adventofcode.com/2025/day/1
+
+// Format of inputs is a newline separated list of a r"(.|@)+"
+// Solution tested with node v22.9.0
+
+const fs = require("fs");
+const path = require("path");
+
+const fileInfo = fs.readFileSync(path.join(__dirname, "input.txt")).toString();
+// const fileInfo = fs .readFileSync(path.join(__dirname, "input_test.txt")) .toString();
+// console.log("fileInfo: ", fileInfo);
+
+const lines = fileInfo.split("\n").map((v) => v.split(""));
+let res = 0;
+
+let oneRemoved = true;
+while (oneRemoved) {
+  oneRemoved = false;
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      if (lines[i][j] == "@") {
+        let neighbours = 0;
+        for (let k = -1; k <= 1; k++) {
+          for (let l = -1; l <= 1; l++) {
+            let inRange =
+              (k != 0 || l != 0) &&
+              i + k >= 0 &&
+              i + k < lines.length &&
+              j + l >= 0 &&
+              j + l < lines[i + k].length;
+            if (inRange && lines[i + k][j + l] == "@") neighbours++;
+          }
+        }
+
+        if (neighbours < 4) {
+          res++;
+          lines[i][j] = ".";
+          oneRemoved = true;
+        }
+      }
+    }
+  }
+}
+
+console.log("result: ", res);
